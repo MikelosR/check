1c5d3d74a2889798791c34bb5f9349cc09de0498
MikelosR <miiikelossspap@outlook.com.gr>
2021-03-04 14:56:24 +0200

    mallon ta katafera

---
+++ didit.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+int main(){
+    printf("ta katafera telika\n");
+}
--- testarisma.c
+++ testarisma.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
 int main( ){
-    printf("testaro");
+    printf("testaro\n");
+    printf("epeksigisi 00\n");
 }
